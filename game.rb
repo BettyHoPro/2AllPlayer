4f8281c0212aaf26c7c18178c90e23fa4c8c9599
BettyHoPro <bettyvisualtech@gmail.com>
2021-03-30 14:08:17 -0700

    feat: check current_player state

--- game.rb
+++ game.rb
@@ -8,11 +8,16 @@ class Game
   def initialize
     @player1 = Player.new("Player 1")
     @player2 = Player.new("Player 2")
-    @current_player = rand(1..2)
+    @current_player = rand(1..2).to_i
   end
 
   def start 
     puts "Game on! Wish you luck"
-    puts "#{@current_player}"
+    # puts "#{@current_player} #{@current_player.class}"
+    if  @current_player == 1 
+      puts "Player 1: your turn to play, press Y to agree"
+    else
+      puts "Player 2: your turn to play, press Y to agree"
+    end
   end
 end 
